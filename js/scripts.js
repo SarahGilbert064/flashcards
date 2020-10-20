88dc8d5e835f0bee54668ad3f1e5e2b0d9c98665
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-10-20 15:13:25 -0700

    create buttons for operator, variables, and variable naming that show their definitions
    Co-authored-by: Brian Sturgis <sturujisu@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
     $(".javascript-show").show();
     $(".javascript-hide").toggle();
   });
+  $("#operator").click(function() {
+    $(".operator-button").show();
+    $(".operator-definition").toggle();
+  });
+  $("#variables").click(function() {
+    $(".variables-button").show();
+    $(".variables-definition").toggle();
+  });
+  $("#variablenames").click(function() {
+    $(".variablenaming-button").show();
+    $(".variablenaming-defintion").toggle();
+  });
 });
+
+  
